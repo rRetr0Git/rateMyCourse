eb3cb9a735fae70910dd112f9b6165bd553c9244
rRetr0Git <wangwenjun1606@buaa.edu.cn>
2019-03-29 21:32:57 +0800

    refactor(models.py):modify new database entities

--- rateMyCourse/admin.py
+++ rateMyCourse/admin.py
@@ -4,10 +4,13 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(School)
-admin.site.register(Department)
+admin.site.register(CourseRate)
+admin.site.register(CourseKeyWord)
 admin.site.register(Teacher)
 admin.site.register(User)
 admin.site.register(Comment)
 admin.site.register(Course)
-admin.site.register(Rate)
+admin.site.register(SchoolCourse)
+admin.site.register(CourseTeacher)
+admin.site.register(CommentUserCourse)
 admin.site.register(HitCount)
--- rateMyCourse/models.py
+++ rateMyCourse/models.py
@@ -1,132 +1,141 @@
 #-*- coding: UTF-8 -*-
 from django.db import models
-
+from django.contrib.auth.hashers import make_password, check_password
 # Create your models here.
 
 class School(models.Model):
     # attributes
-    name = models.CharField(max_length=50)
+    id = models.UUIDField(primary_key=True,editable=False)
+    name = models.CharField(max_length=30)
+    status = models.IntegerField(default=0)
 
     def __str__(self):
     	return self.name
-
-class Department(models.Model):
-    # attributes
-    name = models.CharField(max_length=50)
-    website = models.URLField(blank=True)
-
-    # connections
-    school = models.ForeignKey(
-        School,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    def __str__(self):
-    	return self.name
-
-class Teacher(models.Model):
-    # attributes
-    name = models.CharField(max_length=50)
-    website = models.URLField(blank=True)
-    
-    # connections
-    department = models.ForeignKey(
-        Department,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    def __str__(self):
-    	return self.name
-
-class User(models.Model):
-    # attributes
-    username = models.CharField(max_length=50, unique=True) # 用户名不可重复
-    mail = models.EmailField(unique=True)
-    password = models.CharField(max_length=50)
-    '''
-    grade = models.CharField(max_length=50)
-    reported = models.BooleanField()
-    '''
-    # connections
-    '''
-    school = models.ForeignKey(
-        School,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    department = models.ForeignKey(
-        Department,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    '''
-    def __str__(self):
-    	return self.username
 
 class Course(models.Model):
     # attributes
-    name = models.CharField(max_length=50)
-    number = models.CharField(max_length=50)
-    website = models.URLField()
-    description = models.CharField(max_length=2000, blank=True)
-    credit = models.FloatField()
-    coursetype = models.CharField(max_length=50)
+    id = models.UUIDField(primary_key=True,editable=False)
+    name = models.CharField(max_length=20)
+    website = models.URLField(null=True)
+    description = models.CharField(max_length=200, null=True)
+    type = models.CharField(max_length=10,null=True)
+    status = models.IntegerField(default=0)
+    def __str__(self):
+    	return self.name
 
-    # connections
-    department = models.ForeignKey(
-        Department,
-        on_delete=models.SET_NULL,
-        null=True,
+class CourseRate(models.Model):
+    # attributes
+    id = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        editable=False
     )
-    teacher_set = models.ManyToManyField(
-        Teacher,
+    score = models.FloatField(default=3.0)
+    homework = models.FloatField(default=3.0)#作业量
+    difficulty = models.FloatField(default=3.0)#难易度
+    knowledge = models.FloatField(default=3.0)#收获度
+    satisfaction = models.FloatField(default=3.0)#满意度
+    rateCount = models.IntegerField(default=0)
+
+class CourseKeyWord(models.Model):
+    # attributes
+    id = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        editable=False
     )
+    keyword1 = models.CharField(max_length=10, null=True)
+    keyword2 = models.CharField(max_length=10, null=True)
+    keyword3 = models.CharField(max_length=10, null=True)
+    keyword4 = models.CharField(max_length=10, null=True)
+    keyword5 = models.CharField(max_length=10, null=True)
+
+class User(models.Model):
+    # attributes
+    id = models.UUIDField(primary_key=True)
+    username = models.CharField(max_length=20, unique=True) # 用户名不可重复
+    password = models.CharField(max_length=50)
+    isTeacher = models.BooleanField(default=False)
+    schoolName = models.CharField(max_length=30,null=True)
+    departmentName = models.CharField(max_length=20,null=True)
+    img = models.URLField(blank=True,null=True)
+    def __str__(self):
+    	return self.username
+
+class Teacher(models.Model):
+    # attributes
+    id = models.UUIDField(primary_key=True,editable=False)
+    name = models.CharField(max_length=10)
+    website = models.URLField(null=True)
+    img = models.URLField(null=True)
+    status = models.IntegerField(default=0)
     def __str__(self):
     	return self.name
 
 class Comment(models.Model):
     # attributes
+    id = models.UUIDField(primary_key=True,editable=False)
     anonymous = models.BooleanField(default=False)
     content = models.CharField(max_length=2000)
     time = models.DateTimeField()
-    # connections
-    parentcomment = models.ForeignKey(
+    isRoot = models.BooleanField(default=True)
+    parentComment = models.ForeignKey(
         'self',
         on_delete=models.SET_NULL,
         null=True,
     )
-    user = models.ForeignKey(
-        User,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    course = models.ForeignKey(
-        Course,
-        on_delete=models.CASCADE,
-    )
-    term = models.CharField(max_length=50)
-    total_score = models.FloatField()
-    
+    img = models.URLField(blank=True,null=True)
+    homework = models.IntegerField(default=3)  # 作业量
+    difficulty = models.IntegerField(default=3)  # 难易度
+    knowledge = models.IntegerField(default=3)  # 收获度
+    satisfaction = models.IntegerField(default=3)  # 满意度
     def __str__(self):
     	return self.content
 
-class Rate(models.Model):
-    # attributes
-    A_score = models.FloatField(default=0) # 有趣程度
-    B_score = models.FloatField(default=0) # 充实程度
-    C_score = models.FloatField(default=0) # 课程难度
-    D_score = models.FloatField(default=0) # 课程收货
-    # connections
-    user = models.ForeignKey(
-        User,
+class SchoolCourse(models.Model):
+    schoolId = models.ForeignKey(
+        School,
         on_delete=models.CASCADE,
     )
-    course = models.ForeignKey(
+    courseId = models.ForeignKey(
         Course,
         on_delete=models.CASCADE,
     )
-    def __str__(self):
-    	return "rate from %s"%self.user
+    class Meta:
+        unique_together = ("schoolId", "courseId")
+
+
+class CourseTeacher(models.Model):
+    courseId = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+    )
+    teacherId = models.ForeignKey(
+        Teacher,
+        on_delete=models.CASCADE,
+    )
+    class Meta:
+        unique_together = ("courseId", "teacherId")
+
+
+class CommentUserCourse(models.Model):
+    commentId = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE,
+    )
+    userId = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    courseId = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+    )
+    class Meta:
+        unique_together = ("commentId", "userId","courseId")
+
 
 class HitCount(models.Model):
     name = models.CharField(max_length=50)
