40f2788b5b42ce28a374cce88c70f11f93b3e675
AlbertShenC <30514009+AlbertShenC@users.noreply.github.com>
2019-04-20 14:09:32 +0800

    modify the interface of comments

--- rateMyCourse/static/ratemycourse/javascript/coursePage.js
+++ rateMyCourse/static/ratemycourse/javascript/coursePage.js
@@ -76,81 +76,87 @@ function generateGrid(imageUrls, userName, text, time) {
         <div>
             <div>
                 <div>
-                    <img>
-                    <p>
-                </div>
-                <div>
-                    <p>
-                </div>
-                <div>
-                </div>
-            </div>
-            <div>
-                <div>
                     <div>
-                        <p>
-                        </p>
+                        <p></p>
                     </div>
                     <div>
+                        <p></p>
                     </div>
                     <div>
-<!--                        <button>-->
-<!--                        </button>-->
+<!--                        <button></button>-->
                     </div>
                     <div>
-<!--                        <button>-->
-<!--                        </button>-->
+<!--                        <button></button>-->
                     </div>
                 </div>
             </div>
         </div>
-        `;
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <img>
+                    </div>
+                    <div>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
 
-        // create div
-        var commentGrid = document.createElement("div");
-        commentGrid.id = "commentGrid";
-        commentGrid.setAttribute("class","list-group-item");
-        commentGrid.innerHTML = ScreenGridHtml;
-        //insert user image and name
-        var imageTag = commentGrid.getElementsByTagName("img");
-        imageTag[0].src = "http://wx2.sinaimg.cn/large/0076t302ly1fylgfkon55j30e80e8ab5.jpg";
-        imageTag[0].width = "50";
-        imageTag[0].height = "50";
-        //imageTag[0].setAttribute("style", "margin-bottom:16px;margin-top:16px");
+    // create div
+    var commentGrid = document.createElement("div");
+    commentGrid.id = "commentGrid";
+    commentGrid.setAttribute("class","list-group-item");
+    commentGrid.innerHTML = ScreenGridHtml;
 
-        var pTags = commentGrid.getElementsByTagName("p");
-        var userNameNode = document.createTextNode(userName);
-        pTags[0].appendChild(userNameNode);
-        //pTags[0].setAttribute("class", "userName");
+    var divTags = commentGrid.getElementsByTagName("div");
+    var pTags = commentGrid.getElementsByTagName("p");
 
-        //insert text
-        pTags[1].innerHTML = text;
-        pTags[1].setAttribute("class", "center-vertical")
-        //inset time
-        var timenode = document.createTextNode(time);
-        pTags[2].appendChild(timenode);
-        //pTags[2].setAttribute("style", "width:100%;text-align:right;margin-top:32px")
-        // var buttonTag = commentGrid.getElementsByTagName("button");
-        // buttonTag[0].type = "button";
-        // buttonTag[0].setAttribute("class","btn btn-sm btn-success");
-        // buttonTag[0].innerHTML = "👍";
-        // buttonTag[1].type = "button";
-        // buttonTag[1].setAttribute("class","btn btn-sm btn-danger");
-        // buttonTag[1].innerHTML = "👎";
-        //css
-        var divTags = commentGrid.getElementsByTagName("div");
-        divTags[0].setAttribute("class", "col-md-12 column");
-        divTags[1].setAttribute("class", "row clearfix");
-        divTags[2].setAttribute("class", "col-md-1 column");
-        divTags[3].setAttribute("class", "col-md-7 column");
-        divTags[4].setAttribute("class", "col-md-4 column");
-        divTags[5].setAttribute("class", "col-md-12 column");
-        divTags[6].setAttribute("class", "row clearfix");
-        divTags[7].setAttribute("class", "col-md-2 column");
-        divTags[8].setAttribute("class", "col-md-6 column");
-        divTags[9].setAttribute("class", "col-md-2 column");
-        divTags[10].setAttribute("class", "col-md-2 column");
-        return commentGrid;
+    divTags[0].setAttribute("class","list-group-item");
+    divTags[1].setAttribute("class","col-md-12 column");
+    divTags[2].setAttribute("class","row clearfix");
+
+    // insert user name
+    divTags[3].setAttribute("class","col-md-4 column");
+    var userNameNode = document.createTextNode(userName);
+    pTags[0].appendChild(userNameNode);
+
+    // insert time
+    divTags[4].setAttribute("class","col-md-4 column")
+    var timenode = document.createTextNode(time);
+    pTags[1].appendChild(timenode);
+
+    var buttonTag = commentGrid.getElementsByTagName("button");
+    // insert vote-up
+    divTags[5].setAttribute("class","col-md-2 column")
+    // buttonTag[0].type = "button";
+    // buttonTag[0].setAttribute("class","btn btn-sm btn-success");
+    // buttonTag[0].innerHTML = "👍";
+
+    // insert vote-down
+    divTags[6].setAttribute("class","col-md-2 column")
+    // buttonTag[1].type = "button";
+    // buttonTag[1].setAttribute("class","btn btn-sm btn-danger");
+    // buttonTag[1].innerHTML = "👎";
+
+    divTags[7].setAttribute("class","list-group-item");
+    divTags[8].setAttribute("class","col-md-12 column");
+    divTags[9].setAttribute("class","row clearfix");
+    // insert picture
+    divTags[10].setAttribute("class","col-md-2 column");
+    var imageTag = commentGrid.getElementsByTagName("img");
+    imageTag[0].src = "http://wx2.sinaimg.cn/large/0076t302ly1fylgfkon55j30e80e8ab5.jpg";
+    imageTag[0].width = "100";
+    imageTag[0].height = "100";
+
+    // insert comment
+    divTags[11].setAttribute("class","col-md-10 column")
+    pTags[2].innerHTML = text;
+    pTags[2].setAttribute("class", "center-vertical")
+
+    return commentGrid;
 }
 
 function setComments() {//get comments list from service
