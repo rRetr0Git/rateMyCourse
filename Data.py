6dda5b820094891b3ce3f2af76a99f21c873f222
rRetr0Git <wangwenjun1606@buaa.edu.cn>
2019-05-06 00:55:34 +0800

    new data & .py for course & teacher

--- Data.py
+++ Data.py
@@ -1,17 +1,19 @@
 import pandas as pd
 from rateMyCourse.models import *
 import string
-
+print('cleaning data......')
 for table in [School, Course, Teacher, SchoolCourse, CourseTeacher]:
 	table.objects.all().delete()
-
+print('cleaning finished!')
+print('importing data......')
+print('please be patient......')
 buaa = School(name="北京航空航天大学")
 buaa.save()
 school_uuid = School.objects.get(name="北京航空航天大学").id
-print(school_uuid)
-df = pd.read_csv(open('filtered.csv',encoding="utf-8"))
-
-for row in df.iterrows():
+#print(school_uuid)
+df1 = pd.read_csv(open('course_data_new.csv',encoding="utf-8"))
+df2 = pd.read_csv(open('teacher.csv',encoding="utf-8"))
+for row in df1.iterrows():
 	row = row[1]
 	name = row['课程名称']
 	department = row['开课院系']
@@ -27,10 +29,22 @@ for row in df.iterrows():
 		if each != "N/A":
 			teacher = Teacher.objects.filter(name=each)
 			if len(teacher)==0:
-				teacher = Teacher(name=each)
+				search = df2.loc[df2['姓名']==each.replace(' ','')]
+				if search.empty!=True:
+					t_row = search.iloc[0]
+					t_website = t_row['个人主页']
+					t_img = t_row['个人照片']
+					teacher = Teacher(name=each,website=t_website,img=t_img)
+					#print(t_website,t_img)
+				else:
+					teacher = Teacher(name=each)
 				teacher.save()
 			else:
 				teacher = teacher[0]
 			teacher_uuid = teacher.id
-			ct = CourseTeacher(courseId=course,teacherId=teacher)
-			ct.save()
+			try:
+				ct = CourseTeacher(courseId=course,teacherId=teacher)
+				ct.save()
+			except:
+				continue
+print('import finished!')
