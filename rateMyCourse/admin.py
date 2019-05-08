722c24deae73e4b6d3632df6f52301e3fd1a7b2e
Niuyuhang03 <niuyuhang03@qq.com>
2019-05-08 12:41:05 +0800

    delete courseRate in admin

--- rateMyCourse/admin.py
+++ rateMyCourse/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(School)
-admin.site.register(CourseRate)
+# admin.site.register(CourseRate)
 admin.site.register(CourseKeyWord)
 admin.site.register(Teacher)
 admin.site.register(User)
